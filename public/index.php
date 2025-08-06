9f959053a0982a6c792caef16b60a7c4b620d61b
akashbhowmik54 <bhowmikakash67@gmail.com>
2025-08-07 00:13:09 +0600

    Complete initial setup

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once '../config/config.php';
+require_once '../app/core/Database.php';
+require_once '../app/core/Controller.php';
+require_once '../app/core/Router.php';
+
+use Core\Router;
+
+$url = $_GET['url'] ?? 'home/index';
+
+$router = new Router();
+$router->direct($url);
